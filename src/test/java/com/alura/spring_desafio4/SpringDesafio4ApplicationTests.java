1d596b62fd9804e96b1eaebf50be6c180f54a36e
aalcarvalho <aalcarvalho@NCELEPAR24498>
2025-05-16 10:21:00 -0300

    Primeiro commit

---
+++ src/test/java/com/alura/spring_desafio4/SpringDesafio4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.spring_desafio4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesafio4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
